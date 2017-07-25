58bd3d6fddb38aa9af7b95dff4bc961aa3df6adb
mkp246 <mkp246@gmail.com>
2017-07-25 18:48:59 +0530

    REFACTOR

--- src/string/string_test.go
+++
@@ -1,7 +0,0 @@
-package string
-
-import "testing"
-
-func Test(t *testing.T)  {
-
-}
---
+++ src/user/hello.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+import "stringutil"
+func main() {
+    fmt.Println(stringutil.Reverse("hello world!!"))
+}
